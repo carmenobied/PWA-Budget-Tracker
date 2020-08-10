e0abffa93ede2daddc2aac99926136a4e3f45a72
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-08-09 22:49:12 -0700

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,9 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/index.js"
+    "/index.js",
+    "/manifest.webmanifest",
+    "/style.css",
   ];
   
   const CACHE_NAME = "static-cache-v2";
